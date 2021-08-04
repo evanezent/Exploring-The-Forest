ed32ffc239b38bce921e82c66007d9a6e8b9af7d
evanezcent <pratamayo16@gmail.com>
2021-08-04 09:22:13 +0700

    animation done

--- lib/ui/screens/schedule_screen.dart
+++ lib/ui/screens/schedule_screen.dart
@@ -22,53 +22,74 @@ class _ScheduleScreenState extends State<ScheduleScreen>
   String choosedExpense = "0";
   late AnimationController _animationController;
 
+  late AnimationController _iconController;
+  late Animation<double> _pulseAnimation;
+
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
+
     _animationController = AnimationController(
       duration: const Duration(milliseconds: 2000),
       vsync: this,
     )..repeat();
+
+    _iconController =
+        AnimationController(vsync: this, duration: const Duration(milliseconds: 1500));
+    _iconController.forward();
+
+    _pulseAnimation = Tween<double>(begin: 0.8, end: 1).animate(
+        CurvedAnimation(parent: _iconController, curve: Curves.easeIn));
+
+    _pulseAnimation.addStatusListener((status) {
+      if (status == AnimationStatus.completed)
+        _iconController.reverse();
+      else if (status == AnimationStatus.dismissed) _iconController.forward();
+    });
   }
 
   @override
   void dispose() {
     _animationController.dispose();
+    _iconController.dispose();
     super.dispose();
   }
 
-  Widget rippleComponents() {
-    return Center(
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(100),
-        child: DecoratedBox(
-          decoration: BoxDecoration(
-            gradient: RadialGradient(
-              colors: <Color>[
-                Color.lerp(Colors.white, custom_blue_disable, 0.6)!,
-                Colors.white,
-              ],
+  Widget rippleComponents(Animation<double> pulse) {
+    return  Center(
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(100),
+          child: Container( 
+            decoration: BoxDecoration(
+              gradient: RadialGradient(
+                colors: <Color>[
+                  Color.lerp(Colors.white, custom_blue_disable, 0.6)!,
+                  Colors.white,
+                ],
+              ),
             ),
-          ),
-          child: ScaleTransition(
+            child: ScaleTransition(
               scale: Tween(begin: 0.95, end: 1.0).animate(
                 CurvedAnimation(
                   parent: _animationController,
                   curve: CurveWave(),
                 ),
               ),
-              child: Container(
-                padding:EdgeInsets.all(2),
-                color: Colors.white,
-                child: Icon(
-                  Icons.calendar_today_sharp,
-                  color: custom_blue,
-                  size: 30,
+              child: ScaleTransition(
+                scale: _pulseAnimation,
+                child: Container(
+                  padding: EdgeInsets.all(2),
+                  color: Colors.white,
+                  child: Icon(
+                    Icons.calendar_today_sharp,
+                    color: custom_blue,
+                    size: 30,
+                  ),
                 ),
-              )),
-        ),
-      ),
+              ),
+            ),
+          ),
+        ), 
     );
   }
 
@@ -92,27 +113,33 @@ class _ScheduleScreenState extends State<ScheduleScreen>
         child: ListView(
           children: [
             Timeline(
-              index: 3,
+              index: 4,
             ),
-            SizedBox(height: 30),
-            Text("Personal Information", style: white_800_20),
+            SizedBox(height: 40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Center(   
+                  child: CustomPaint(
+                    painter: CirclePainter(
+                      _animationController,
+                      color: Colors.white,
+                    ),
+                    child: SizedBox(
+                      width: 100,
+                      height: 100,
+                      child: rippleComponents(_pulseAnimation),
+                    ),
+                  ),
+                ),
+              ],
+            ), 
+            Text("Schedule Video Call", style: white_800_20),
             SizedBox(height: 6),
             Text(
-                "Please fill in the information below and your goal for digital saving.",
+                "Choose the date and time you preferred, we will send a link via email to make a video call on the scheduled date and time.",
                 style: white_normal_14),
             SizedBox(height: 30),
-            Center(
-                child: CustomPaint(
-              painter: CirclePainter(
-                _animationController,
-                color: Colors.white,
-              ),
-              child: SizedBox(
-                width: 100,
-                height: 100,
-                child: rippleComponents(),
-              ),
-            )),
             Container(
               height: 50,
               margin: EdgeInsets.only(top: 20),
